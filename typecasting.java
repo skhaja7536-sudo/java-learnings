d5173910c324861b8e70a27495013b6ee19155c7
shaik khaja <skhaja7536@gmail.com>
2026-01-15 21:38:31 +0530

    Add files via upload

---
+++ typecasting.java
@@ -0,0 +1,14 @@
+
+public class typecasting {
+
+	public static void main(String[] args) {
+		// TODO87 Auto-generated method stub
+		char A = 77;
+		float b;
+		b=A;
+		System.out.println(A);
+		System.out.println(b);
+		
+	}
+
+}
